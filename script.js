382253ed9de8942a79b1f3660371f38cd19cc04c
Yuxuan <nine.slide@gmail.com>
2022-09-22 15:43:30 +0200

    run promises parallelly

--- script.js
+++ script.js
@@ -201,7 +201,34 @@ const whereAmI2 = async function () {
     } catch (err) {
         console.log(`${err.message} 🛸`);
         renderError(`🎶 ${err.message}`);
+
+        throw err; //' rethrow the error
     }
 };
+//! async function always returns a fullfilled promise, to avoid this, rethrow the error again in the catch
+
 // immediate display current location
 whereAmI2();
+
+//> async in parallel
+const getJSON = function (url, errMsg = 'something went wrong') {
+    return fetch(url).then((res) => {
+        if (!res.ok) throw new Error(`${errMsg} (${res.status})`);
+
+        return res.json();
+    });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+    try {
+        const data = await Promise.all([
+            getJSON(`https://restcountries.com/v2/name/${c1}`),
+            getJSON(`https://restcountries.com/v2/name/${c2}`),
+            getJSON(`https://restcountries.com/v2/name/${c3}`),
+        ]); //' promise.all shortcircuits when one promise is rejected. so only run it when they are not codependent
+        console.log(data);
+        console.log(data.map((d) => d[0].capital));
+    } catch (err) {
+        console.error(err.message);
+    }
+};
